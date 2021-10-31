593bb8de99c5968c805495af28e690f486c3e85f
Joe <joeleblanc920@gmail.com>
2021-10-30 19:17:02 -0700

    Completed assignment

---
+++ index.js
@@ -0,0 +1,9 @@
+function introduction(name ="Joe") {
+    return(`Hi, my name is ${name}.`);
+}
+function introductionWithLanguage(name="Bob", language="Ember.js"){
+    return(`Hi, my name is ${name} and I am learning to program in ${language}.`);
+}
+function introductionWithLanguageOptional(name="Gracie", language="JavaScript"){
+    return(`Hi, my name is ${name} and I am learning to program in ${language}.`);
+}
